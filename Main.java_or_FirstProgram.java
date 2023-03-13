2fbe73978d749e42e35587653a35203a7a858034
sj1702 <107590591+sj1702@users.noreply.github.com>
2023-03-13 13:20:17 +0530

    Create Main.java_or_FirstProgram.java
    Every line of code that runs in Java must be inside a class. In our example, we named the class Main. A class should always start with an uppercase first letter.
    Note: Java is case-sensitive: "MyClass" and "myclass" has different meaning.
    The name of the java file must match the class name. When saving the file, save it using the class name and add ".java" to the end of the filename.

---
+++ Main.java_or_FirstProgram.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+
+
+
+OR
+
+
+class FirstProgram{
+     public static void main(String[] args){
+         System.out.println("Hello, World!");
+     }
+ }
